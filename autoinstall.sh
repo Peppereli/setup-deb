e6a1db10ed9bf5128fd87ad89d89b304ed8814d1
Peeps <lastf8474@gmail.com>
2025-08-03 18:16:09 +0500

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -9,7 +9,7 @@ sudo apt update
 echo "INSTALLING PACKAGES CORE PACKAGES..."
 sudo apt install --no-install-recommends nala -y
 sudo nala install --no-install-recommends alacritty light sway swaybg swayidle swayimg swaylock waybar fonts-font-awesome fastfetch rofi curl libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-gtk3 modemmanager network-manager network-manager-gnome iwd bleachbit pavucontrol mtp-tools gvfs-fuse gvfs-backends nwg-look lxpolkit dunst btop dysk gcc g++ fzf zsh sxiv zip unzip thunar thunar-volman thunar-archive-plugin gvfs mpv xarchiver tar 7zip x11-xserver-utils tumbler geany ffmpegthumbnailer thunar-media-tags-plugin cmus upower acpi -y
-sudo nala install transmission-gtk grim slurp wl-clipboard neovim pipewire pipewire-pulse xwayland xdg-desktop-portal-wlr zathura zathura-pdf-poppler zathura-cb fonts-noto -y
+sudo nala install qbittorrent grim slurp wl-clipboard neovim pipewire pipewire-pulse xwayland xdg-desktop-portal-wlr zathura zathura-pdf-poppler zathura-cb fonts-noto -y
 
 echo "ADDING 32-BIT ARCHITECTURE SUPPORT..."
 sudo dpkg --add-architecture i386
