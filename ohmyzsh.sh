d3c46e2ff87f486b6c1112766980622af15a86b5
Peeps <lastf8474@gmail.com>
2025-08-05 00:17:41 +0500

    Create ohmyzsh.sh

---
+++ ohmyzsh.sh
@@ -0,0 +1,5 @@
+echo "INSTALLING OH MY ZSH!..."
+sudo apt install --no-install-recommends zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
