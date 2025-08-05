74780970d786e5383e87e71ea4cbcade32561062
Peeps <lastf8474@gmail.com>
2025-08-05 11:33:55 +0500

    Update ohmyzsh.sh

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -1,5 +1,6 @@
 echo "INSTALLING OH MY ZSH!..."
 sudo apt install --no-install-recommends zsh -y
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+cd
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
