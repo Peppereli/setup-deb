dd407a1e870ee461ad8bb5522a8ee8ef6d2eb31d
Peeps <lastf8474@gmail.com>
2025-08-05 11:44:08 +0500

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 #https://chromewebstore.google.com/detail/ublock-origin/cjpalhdlnbpafiamejdnhcphjbkeiagm?hl=en-GB
+cd
 echo "WELCOME TO PEEPS' DEBIAN SETUP WIZARD! JUST SIT BACK AND RELAX WHILE THIS THING OF OURS DOES ITS THING."
 
 echo "PLEASE ENTER YOUR LOGIN PASSWORD AND LET IT DO ITS THING"
 
 echo "UPDATING REPOSITORIES..."
 sudo apt update
-
-echo "INSTALLING PACKAGES CORE PACKAGES..."
-sudo apt install --no-install-recommends nala -y
-sudo nala install --no-install-recommends alacritty light sway swaybg swayidle swaylock waybar grim slurp fonts-font-awesome fzf fastfetch rofi wofi curl libreoffice-writer libreoffice-calc libreoffice-impress libreoffice-gtk3 modemmanager network-manager network-manager-gnome iwd bleachbit pavucontrol mtp-tools gvfs-fuse gvfs-backends nwg-look lxpolkit dunst btop dysk gcc zsh sxiv zip unzip thunar thunar-volman thunar-media-tags-plugin thunar-archive-plugin gvfs mpv xarchiver tar 7zip x11-xserver-utils tumbler geany ffmpegthumbnailer cmus upower acpi g++ pipewire wireplumber pipewire-pulse qbittorrent qt6-wayland neovim xwayland xdg-desktop-portal-wlr zathura zathura-pdf-poppler zathura-cb fonts-noto fonts-noto-cjk fonts-noto-color-emoji xwaylandvideobridge -y
 
 echo "ADDING 32-BIT ARCHITECTURE SUPPORT..."
 sudo dpkg --add-architecture i386
